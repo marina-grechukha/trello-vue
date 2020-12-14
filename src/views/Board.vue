aef802dfa1fd65c96c4ce87a537340355a6d2c7c
marina.grechukha <marina.grechukha@gmail.com>
2020-12-14 11:19:50 +0200

    Adding and Editing tasks

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -26,6 +26,13 @@
             </p>
           </div>
         </div>
+
+        <input
+          type="text"
+          class="block p-2 w-full bg-transparent"
+          placeholder="+ Enter new task"
+          @keyup.enter="createTask($event, column.tasks)"
+        />
       </div>
     </div>
 
@@ -55,6 +62,13 @@ export default {
     },
     close () {
       this.$router.push({ name: 'board' })
+    },
+    createTask (e, tasks) {
+      this.$store.commit('CREATE_TASK', {
+        tasks,
+        name: e.target.value
+      })
+      e.target.value = ''
     }
   }
 }
